b5784b8f57331acca4ae04ef25fe45451d67415f
Lothering0 <danil2005232@gmail.com>
2021-12-04 15:30:11 +0300

    Added some images and reworked header button

---
+++ components/Button.vue
@@ -0,0 +1,57 @@
+<template>
+  <button>
+    <div class="constellation" id="constellation-1"></div>
+    Start
+    <div class="constellation" id="constellation-2"></div>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+button {
+  position: relative;
+
+  width: 261px;
+  height: 104px;
+
+  margin-top: 136px;
+
+  background: linear-gradient(93.82deg, #3342AE 53.7%, #314DC9 71.4%);
+  border: 3px solid #fff;
+  border-radius: 7px;
+
+  color: #fff;
+  font-size: 58px;
+  font-weight: 200;
+
+  transition: 0.3s;
+  backface-visibility: hidden;
+
+  &:hover {
+    border: 5px solid #fff;
+    cursor: pointer;
+  }
+}
+
+.constellation {
+  position: absolute;
+
+  width: 102px;
+  height: 102px;
+
+  background-size: cover;
+}
+
+#constellation-1 {
+  top: -40px;
+  left: -48px;
+
+  background-image: url('@/static/constellation-1.png');
+}
+
+#constellation-2 {
+  top: 53px;
+  right: -41px;
+
+  background-image: url('@/static/constellation-2.png');
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="block">
       <div class="header">
         <h1>Explore the space</h1>
-        <button>Start</button>
+        <Button />
       </div>
     </div>
 
@@ -31,29 +31,7 @@
 
     padding-top: 196px;
     padding-left: 172px;
-  }
-}
-
-button {
-  width: 261px;
-  height: 104px;
-
-  margin-top: 136px;
-
-  background: transparent;
-  border: 3px solid #fff;
-  border-radius: 7px;
-
-  color: #fff;
-  font-size: 58px;
-  font-weight: 200;
-
-  transition: 0.6s;
-
-  &:hover {
-    background: #fff;
-    color: #403c63;
-    cursor: pointer;
+    padding-bottom: 55px;
   }
 }
 </style>
